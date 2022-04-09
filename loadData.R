f41fbb1ca3aac1f7742c3f7872abd989f246ee92
Andrew Ma <andrewm2010796@gmail.com>
2022-04-09 15:59:13 -0700

    summary stats tables

--- loadData.R
+++ loadData.R
@@ -1,3 +1,17 @@
 library(haven)
-data <- read_dta('114542-V1/PisoFirme_AEJPol-20070024_household.dta')
-data2 <- read_dta('114542-V1/PisoFirme_AEJPol-20070024_individual.dta')
+library(skimr)
+
+house <- read_dta('114542-V1/PisoFirme_AEJPol-20070024_household.dta')
+indiv <- read_dta('114542-V1/PisoFirme_AEJPol-20070024_individual.dta')
+
+
+test <- house[rowSums(is.na(house)) > 0,]
+test2 <- indiv[rowSums(is.na(indiv)) > 0,]
+
+
+tt <- skim(indiv)
+print(tt)
+
+
+tt <- skim(house)
+print(tt)
