7765841a26586b7e2f552fc301b25338ac45d279
hirosme <hirosme@uw.edu>
2022-05-02 14:13:39 -0700

    built lm models

--- replication.R
+++ replication.R
@@ -1,69 +1,111 @@
 # package & data imports
 library(haven)
-library(dplyr)
+library(tidyverse)
+
+setwd('C:/Users/hiros/Desktop/Storage/documents/linear models/project/stat230-final')
+
+
 house <- read_dta('data/PisoFirme_AEJPol-20070024_household.dta')
 indiv <- read_dta('data/PisoFirme_AEJPol-20070024_individual.dta')
+df = left_join(house,indiv)
+
+dpisofirme = c('dpisofirme')
 
 ## variable definitions for household dataframe
-HH_census <- house[,grep("C_*", colnames(house))] 
-HH_survey <- house[c("S_HHpeople", "S_headage", "S_spouseage", "S_headeduc", "S_spouseeduc",
-                     "S_rooms", "S_waterland", "S_waterhouse", "S_electricity", "S_cementfloor2000",
-                     "S_hasanimals", "S_animalsinside", "S_garbage", "S_washhands",
-                     "S_incomepc", "S_assetspc", "S_shpeoplework", "S_microenter", "S_hrsworkedpc", "S_consumptionpc" ,
-                     "S_cashtransfers", "S_milkprogram", "S_foodprogram")]
-HH_demog1 <- house[c("S_HHpeople", "S_headage", "S_spouseage", "S_headeduc", "S_spouseeduc")]
-HH_demog2 <- house[,grep("S_dem*", colnames(house))] 
-HH_health <- house[c("S_waterland", "S_waterhouse", "S_electricity", "S_hasanimals", 
-                     "S_animalsinside", "S_garbage", "S_washhands")]
-HH_econ <- house[c("S_incomepc","S_assetspc")]
-HH_social <- house[c("S_cashtransfers", "S_milkprogram", "S_foodprogram", "S_seguropopular")]
-HH_floor <- house[c("S_shcementfloor", "S_cementfloorkit", 
-                    "S_cementfloordin", "S_cementfloorbat", "S_cementfloorbed")]
-HH_satis <- house[c("S_satisfloor", "S_satishouse", "S_satislife", "S_cesds", "S_pss")]
-HH_robust <- house[c("S_instcement", "S_instsanita", "S_restsanita", "S_constceili",
-                    "S_restowalls", "S_improveany", "S_logrent", "S_logsell", "S_consumptionpc")]
+HH_census <- grep("C_*", colnames(df),value = T)
+HH_survey <- c("S_HHpeople", "S_headage", "S_spouseage", "S_headeduc", "S_spouseeduc",
+               "S_rooms", "S_waterland", "S_waterhouse", "S_electricity", "S_cementfloor2000",
+               "S_hasanimals", "S_animalsinside", "S_garbage", "S_washhands",
+               "S_incomepc", "S_assetspc", "S_shpeoplework", "S_microenter", "S_hrsworkedpc", "S_consumptionpc" ,
+               "S_cashtransfers", "S_milkprogram", "S_foodprogram")
+HH_demog1 <- c("S_HHpeople", "S_headage", "S_spouseage", "S_headeduc", "S_spouseeduc")
+HH_demog2 <- grep("S_dem*", colnames(df),value = T)
+HH_health <- c("S_waterland", "S_waterhouse", "S_electricity", "S_hasanimals", 
+               "S_animalsinside", "S_garbage", "S_washhands")
+HH_econ <- c("S_incomepc","S_assetspc")
+HH_social <- c("S_cashtransfers", "S_milkprogram", "S_foodprogram", "S_seguropopular")
+HH_floor <- c("S_shcementfloor", "S_cementfloorkit", 
+              "S_cementfloordin", "S_cementfloorbat", "S_cementfloorbed")
+HH_satis <- c("S_satisfloor", "S_satishouse", "S_satislife", "S_cesds", "S_pss")
+HH_robust <- c("S_instcement", "S_instsanita", "S_restsanita", "S_constceili",
+               "S_restowalls", "S_improveany", "S_logrent", "S_logsell", "S_consumptionpc")
+CH_survey <- c("S_age", "S_gender", "S_childma", "S_childmaage", "S_childmaeduc", 
+               "S_childpa", "S_childpaage", "S_childpaeduc")
+CH_demog <- c("S_HHpeople", "S_rooms", "S_age", "S_gender", "S_childma", "S_childmaage", 
+              "S_childmaeduc", "S_childpa", "S_childpaage", "S_childpaeduc")
+CH_health <- c("S_parcount", "S_diarrhea", "S_anemia", 
+               "S_mccdts", "S_pbdypct", "S_haz", "S_whz")
+CH_robust <- c("S_respira", "S_skin", "S_otherdis")
+PA_robust <- c("S_malincom", "S_palincom")
+dtriage = grep('dtriage?',colnames(df),value = T)
+Ex_cols = c('dpisofirme','idcluster','coord_x','coord_y','idmun','idmza')
+
+## Missing Value Imputations
+
+Impute_df = function(df) {
+  dmiss = apply(df,2,function(x) {
+    return(is.na(x))
+  }) %>% cbind.data.frame()
+  
+  colnames(dmiss) = paste0('dmiss_',colnames(df))
+  
+  df[is.na(df)] = 0
+  
+  return(cbind(df,dmiss))
+}
+
+imp_cols = c(HH_demog1,HH_demog2,HH_health,HH_econ,HH_social,CH_demog)
+non_imp_cols = c(Ex_cols,HH_census,HH_survey,HH_floor,HH_satis,HH_robust,CH_survey,
+                 CH_health,CH_robust,PA_robust,dtriage)
+df_imp = cbind(df[,non_imp_cols],Impute_df(df[,imp_cols]))
+
+HH_demog1_imp = c(HH_demog1,paste0('dmiss_',HH_demog1))
+HH_demog2_imp = c(HH_demog2,paste0('dmiss_',HH_demog2))
+HH_health_imp = c(HH_health,paste0('dmiss_',HH_health))
+HH_econ_imp = c(HH_econ,paste0('dmiss_',HH_econ))
+HH_social_imp = c(HH_social,paste0('dmiss_',HH_social))[1:5]
+CH_demog_imp = c(CH_demog,paste0('dmiss_',CH_demog))
+
+## Defining Models for Regressions
+
+HHmodel_1_control = dpisofirme
+HHmodel_2_control = c(dpisofirme,HH_demog1_imp,HH_demog2_imp,HH_health_imp)
+HHmodel_3_control = c(dpisofirme,HH_demog1_imp,HH_demog2_imp,HH_health_imp,
+                      HH_social_imp)
+HHmodel_4_control = c(dpisofirme,HH_demog1_imp,HH_demog2_imp,HH_health_imp,
+                      HH_social_imp,HH_econ_imp)
+
+INmodel_1_control = dpisofirme
+INmodel_2_control = c(dpisofirme,CH_demog_imp,dtriage,HH_health_imp)
+INmodel_3_control = c(dpisofirme,CH_demog_imp,dtriage,HH_health_imp,
+                      HH_social_imp)
+INmodel_4_control = c(dpisofirme,CH_demog_imp,dtriage,HH_health_imp,
+                      HH_social_imp,HH_econ_imp)
 
 ## Table 1: Description of Outcome Variables and Sample Sizes in 2005 Survey
 #tabstat $HH_floor $HH_satis $HH_robust if idcluster!=., by(dpisofirme) s(count);
-t1 <- cbind(house$idcluster, house$dpisofirme, HH_floor,HH_satis, HH_robust) %>%
-  na.rm()
-
-## Table 2: Difference of Means for Pre-intervention 2000 Census Variables
-t2 <- 
-
-## Table 3: Difference of Means for Independent Variables in 2005 Survey
-t3
-
-## Table 4: Cement Floor Coverage Measures
-t4
-
-## Table 5: N/A
-  
-## Table 6: Satisfaction and Maternal Mental Health Measures
-
-  
-## Table 7: Robustness Checks
-
-
-## variable definitions for individual-level dataframe
-CH_survey <- indiv[c("S_age", "S_gender", "S_childma", "S_childmaageå", "S_childmaeduc", 
-                     "S_childpa", "S_childpaage", "S_childpaeduc")]
-CH_demog <- indiv[c("S_HHpeople", "S_rooms", "S_age", "S_gender", "S_childma", "S_childmaage", 
-              "S_childmaeduc", "S_childpa", "S_childpaage", "S_childpaeduc")]
-CH_health <- indiv[c("S_parcount", "S_diarrhea", "S_anemia", 
-               "S_mccdts", "S_pbdypct", "S_haz", "S_whz")]
-CH_robust <- indiv[c("S_respira", "S_skin", "S_otherdis")]
-PA_robust <- indiv[c("S_malincom", "S_palincom")]
+# t1 <- cbind(df$idcluster, df$dpisofirme, HH_floor,HH_satis, HH_robust) %>%
+#   na.rm()
 
 ## Table 1: Description of Outcome Variables and Sample Sizes in 2005 Survey
 #tabstat $CH_health $CH_robust $PA_robust if idcluster!=., by(dpisofirme) s(count);
-t1 <- cbind(indiv$dpisofirme, indiv$idcluster, CH_health, CH_robust, PA_robust)
+# t1 <- cbind(df$dpisofirme, df$idcluster, CH_health, CH_robust, PA_robust)
 
-
-## Table 2: N/A
-## Table 3: Difference of Means for Independent Variables in 2005 Survey
 ## Table 4: Difference of Means for Independent Variables in 2005 Survey
 ## Table 5: Child Health Measures
+
+for (i in CH_health) {
+  outcome = i
+  for (j in 1:4) {
+    control = get(paste0('INmodel_',j,'_control')) %>% paste(collapse = " + ")
+    form = paste0(outcome,'~',control) %>% as.formula
+    assign(paste0(outcome,
+                  '_INmodel_',
+                  j), 
+           lm(form, df_imp,na.action = na.omit))
+  }
+}
+
 ## Table 6: N/A
 ## Table 7: Robustness Checks
 
